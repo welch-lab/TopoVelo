1. Changed simple message passing to GCN 2. Fixed bugs in the training pipeline. Training runs now, but is numerically unstable. Needs further tuning.

topovelo/model/training_data.py
@@ -1,6 +1,9 @@
 import numpy as np
+import torch
 from torch.utils.data import Dataset
-from .model_util import get_neigh_index
+from torch_geometric.data import Data
+# from torch_geometric.loader import NeighborLoader
+
 
 class SCData(Dataset):
     """This is a simple pytorch dataset class for batch training.
@@ -105,51 +108,28 @@ class SCTimedData(Dataset):
                 idx)
 
 
-class SCGraphData(Dataset):
+class SCGraphData():
     """
-    This class wraps around SCData to include graph structured datasets.
-    In particular, it contains a train-test split of nodes (cells), while
-    keeping all the edges.
+    This class wraps around torch_geometric.data to include graph structured datasets.
     """
-    def __init__(self, data, labels, graph, n_train, step=1,  u0=None, s0=None, t0=None, weight=None, seed=2022):
+    def __init__(self, data, labels, graph, n_train, device, seed=2022):
         self.N, self.G = data.shape[0], data.shape[1]//2
-        self.step = 1
-        self.labels = labels
+        self.data = Data(x=torch.tensor(data, dtype=torch.float32),
+                         edge_index=torch.tensor(np.stack(graph.nonzero()), dtype=torch.long),
+                         y=torch.tensor(labels, dtype=torch.long)).to(device)
+        self.edge_weight=torch.tensor(graph.data, dtype=torch.float32)
         np.random.seed(seed)
-        n = data.shape[0]
-        self.node_features = data
-        rand_perm = np.random.permutation(n)
-        self.train_idx = rand_perm[:n_train]
-        self.test_idx = rand_perm[n_train:]
+        rand_perm = np.random.permutation(self.N)
+        self.train_idx = torch.tensor(rand_perm[:n_train], dtype=torch.long).to(device)
+        self.test_idx = torch.tensor(rand_perm[n_train:], dtype=torch.long).to(device)
         self.n_train = n_train
         self.n_test = self.N - self.n_train
 
-        self.neighbor_indices, self.degrees, self.edge_weights = get_neigh_index(graph)
-        self.k = self.neighbor_indices.shape[1]
-        self.u0 = u0
-        self.s0 = s0
-        self.t0 = t0
-        self.weight = np.ones((n_train, self.G)) if weight is None else weight
+        self.u0 = None
+        self.s0 = None
+        self.t0 = None
+        self.u1 = None
+        self.s1 = None
+        self.t1 = None
+
         return
-
-    def __len__(self):
-        return len(self.train_idx)
-
-    def __getitem__(self, idx):
-        sample_idx = self.train_idx[idx]
-        neigh_idx = self.neighbor_indices[sample_idx]
-        if self.u0 is not None and self.s0 is not None and self.t0 is not None:
-            return (self.node_features[sample_idx],
-                    self.node_features[neigh_idx, :],
-                    self.labels[sample_idx],
-                    self.weight[idx],
-                    sample_idx,
-                    self.u0[sample_idx],
-                    self.s0[sample_idx],
-                    self.t0[sample_idx])
-
-        return (self.node_features[sample_idx],
-                self.node_features[neigh_idx, :],
-                self.labels[sample_idx],
-                self.weight[idx],
-                sample_idx)

topovelo/model/vanilla_vae.py
@@ -237,7 +237,6 @@ class VanillaVAE():
                  init_method="steady",
                  init_key=None,
                  tprior=None,
-                 time_distribution="gaussian",
                  checkpoints=None):
         """VeloVAE with latent time only
 
@@ -257,8 +256,6 @@ class VanillaVAE():
         tprior : str, optional
             key in adata.obs that stores the capture time.
             Used for informative time prior
-        time_distribution : str, optional
-            Should be either "gaussian" or "uniform.
         checkpoints : string list
             Contains the path to saved encoder and decoder models. Should be a .pt file.
         """
@@ -291,8 +288,6 @@ class VanillaVAE():
             "train_test_split": 0.7,
             "k_alt": 1,
             "neg_slope": 0.0,
-            "train_scaling": False,
-            "train_std": False,
             "weight_sample": False,
 
             # Plotting
@@ -317,9 +312,8 @@ class VanillaVAE():
         except IndexError:
             print('Please provide two dimensions!')
         self.tmax = torch.tensor(tmax, device=self.device)
-        self.time_distribution = time_distribution
         # Time prior
-        self.get_prior(adata, time_distribution, tmax, tprior)
+        self.get_prior(adata, tmax, tprior)
         # class attributes for training
         self.loss_train, self.loss_test = [], []
         self.counter = 0  # Count the number of iterations
@@ -327,64 +321,36 @@ class VanillaVAE():
         self.train_stage = 1
         self.timer = time.time() - t_start
 
-    def get_prior(self, adata, time_distribution, tmax, tprior=None):
+    def get_prior(self, adata, tmax, tprior=None):
         """Compute the parameters of time prior distribution
 
         Arguments
         ---------
         adata : :class:`anndata.AnnData`
-        time_distribution : {'gaussian', 'uniform'}
         tmax : float
             Maximum time
         tprior : str, optional
             Key in adata.obs storing the capture time
         """
-        if time_distribution == "gaussian":
-            print("Gaussian Prior.")
-            self.kl_time = kl_gaussian
-            self.sample = self.reparameterize
-            if tprior is None:
-                self.p_t = torch.stack([torch.ones(adata.n_obs, 1, device=self.device)*tmax*0.5,
-                                        torch.ones(adata.n_obs, 1, device=self.device)*tmax
-                                        * self.config["time_overlap"]]).float()
-            else:
-                print('Using informative time prior.')
-                t = adata.obs[tprior].to_numpy()
-                t = t/t.max()*tmax
-                t_cap = np.sort(np.unique(t))
-                std_t = np.zeros((len(t)))
-                std_t[t == t_cap[0]] = (t_cap[1] - t_cap[0])*(0.5+0.5*self.config["time_overlap"])
-                for i in range(1, len(t_cap)-1):
-                    std_t[t == t_cap[i]] = 0.5*(t_cap[i] - t_cap[i-1])*(0.5+0.5*self.config["time_overlap"]) \
-                        + 0.5*(t_cap[i+1] - t_cap[i])*(0.5+0.5*self.config["time_overlap"])
-                std_t[t == t_cap[-1]] = (t_cap[-1] - t_cap[-2])*(0.5+0.5*self.config["time_overlap"])
-                self.p_t = torch.stack([torch.tensor(t, device=self.device).view(-1, 1),
-                                        torch.tensor(std_t, device=self.device).view(-1, 1)]).float()
+        self.kl_time = kl_gaussian
+        self.sample = self.reparameterize
+        if tprior is None:
+            self.p_t = torch.stack([torch.ones(adata.n_obs, 1, device=self.device)*tmax*0.5,
+                                    torch.ones(adata.n_obs, 1, device=self.device)*tmax
+                                    * self.config["time_overlap"]]).float()
         else:
-            print("Tailed Uniform Prior.")
-            self.kl_time = kl_uniform
-            self.sample = self.reparameterize_uniform
-            if tprior is None:
-                self.p_t = torch.stack([torch.zeros(adata.n_obs, 1, device=self.device),
-                                        torch.ones(adata.n_obs, 1, device=self.device)*tmax]).float()
-            else:
-                print('Using informative time prior.')
-                t = adata.obs[tprior].to_numpy()
-                t = t/t.max()*tmax
-                t_cap = np.sort(np.unique(t))
-                t_start = np.zeros((len(t)))
-                t_end = np.zeros((len(t)))
-                for i in range(len(t_cap)-1):
-                    t_end[t == t_cap[i]] = t_cap[i] + (t_cap[i+1] - t_cap[i])*(0.5+0.5*self.config["time_overlap"])
-                t_end[t == t_cap[-1]] = t_cap[-1] + (t_cap[-1] - t_cap[-2])*(0.5+0.5*self.config["time_overlap"])
-
-                for i in range(1, len(t_cap)):
-                    t_start[t == t_cap[i]] = max(0, t_cap[i] - (t_cap[i] - t_cap[i-1])
-                                                 * (0.5+0.5*self.config["time_overlap"]))
-                t_start[t == t_cap[0]] = max(0, t_cap[0] - (t_cap[1] - t_cap[0]) *
-                                             (0.5+0.5*self.config["time_overlap"]))
-                self.p_t = torch.stack([torch.tensor(t, device=self.device).unsqueeze(-1),
-                                        torch.tensor(t_end, device=self.device).unsqueeze(-1)]).float()
+            print('Using informative time prior.')
+            t = adata.obs[tprior].to_numpy()
+            t = t/t.max()*tmax
+            t_cap = np.sort(np.unique(t))
+            std_t = np.zeros((len(t)))
+            std_t[t == t_cap[0]] = (t_cap[1] - t_cap[0])*(0.5+0.5*self.config["time_overlap"])
+            for i in range(1, len(t_cap)-1):
+                std_t[t == t_cap[i]] = 0.5*(t_cap[i] - t_cap[i-1])*(0.5+0.5*self.config["time_overlap"]) \
+                    + 0.5*(t_cap[i+1] - t_cap[i])*(0.5+0.5*self.config["time_overlap"])
+            std_t[t == t_cap[-1]] = (t_cap[-1] - t_cap[-2])*(0.5+0.5*self.config["time_overlap"])
+            self.p_t = torch.stack([torch.tensor(t, device=self.device).view(-1, 1),
+                                    torch.tensor(std_t, device=self.device).view(-1, 1)]).float()
 
     def set_device(self, device):
         """Set the device of the model.
@@ -539,11 +505,6 @@ class VanillaVAE():
             else:
                 self.config[key] = config[key]
                 print(f"Warning: unknown hyperparameter: {key}")
-        if self.config["train_scaling"]:
-            self.decoder.scaling.requires_grad = True
-        if self.config["train_std"]:
-            self.decoder.sigma_u.requires_grad = True
-            self.decoder.sigma_s.requires_grad = True
 
     def split_train_test(self, N):
         # Randomly select indices as training samples.
@@ -617,10 +578,6 @@ class VanillaVAE():
         print("*********                 Creating optimizers                 *********")
         param_nn = list(self.encoder.parameters())
         param_ode = self.decoder.get_ode_param_list()
-        if self.config['train_scaling']:
-            param_ode = param_ode+[self.decoder.scaling]
-        if self.config['train_std']:
-            param_ode = param_ode+[self.decoder.sigma_u, self.decoder.sigma_s]
 
         optimizer = torch.optim.Adam(param_nn, lr=self.config["learning_rate"], weight_decay=self.config["lambda"])
         optimizer_ode = torch.optim.Adam(param_ode, lr=self.config["learning_rate_ode"])
@@ -1124,7 +1081,6 @@ class CycleVAE(VanillaVAE):
             "k_alt": 1,
             "neg_slope": 0.0,
             "train_scaling": False,
-            "train_std": False,
             "weight_sample": False,
 
             # Plotting