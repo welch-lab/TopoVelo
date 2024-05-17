e7a342594ad62b42207be0045761a6e38db9353b
Yichen Gu <gyichen@gl-login3.arc-ts.umich.edu>
2024-05-17 09:13:20 -0400

    added setup.py

---
+++ topovelo/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='topovelo',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Yichen Gu',
+    author_email='gyichen@umich.edu',
+    description='topological velocity inference from single-cell spatial transcriptomic data',
+    install_requires=[
+        'anndata>=0.8.0',
+        'hnswlib>=0.6.2',
+        'igraph>=0.10.4',
+        'ipykernel',
+        'ipython',
+        'loess>=2.1.2',
+        'matplotlib>=3.3.0',
+        'memory_profiler',
+        'numba>=0.41.0',
+        'numpy>=1.23.0',
+        'pandas>=0.23',
+        'pynndescent>=0.5.7',
+        'scanpy>=1.5',
+        'scikit-learn>=0.20',
+        'scipy>=1.13.0',
+        'scvelo>=0.2.3, <=0.2.5',
+        'seaborn>=0.10.0',
+        'torch>=1.8.0',
+        'torch_geometric>=2.0.4',
+        'tqdm<=4.62.3',
+        'umap-learn>=0.3.10',
+    ],
+    python_requires='>=3.9'
+)
